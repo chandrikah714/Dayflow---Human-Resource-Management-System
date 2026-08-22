1f23fab80d64bfef3e5034d3fe73c808b1a61e0e
Jeevitha P <jeevip2005@gmail.com>
2026-08-22 17:29:09 +0530

    Delete src directory

--- src/components/ui/Label.jsx
+++
@@ -1,9 +0,0 @@
-import { cn } from '@/lib/utils';
-
-export function Label({ className, children, ...props }) {
-  return (
-    <label className={cn('text-xs font-semibold text-[hsl(var(--foreground))]', className)} {...props}>
-      {children}
-    </label>
-  );
-}
